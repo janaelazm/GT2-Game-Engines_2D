45749b95612fa6b3321cc60a5abcd243b40bba00
Billy Julian Lesmana <hunter140797@gmail.com>
2022-11-11 15:34:47 +0100

    implemented basic WASD movemment with input system

---
+++ GT2-Game/Assets/Billy Work/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Transform playerTransform;
+    private Vector2 movementInput;
+    [SerializeField] private float movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerTransform = GetComponent<Transform>();
+    }
+
+    private void OnMove(InputValue inputValue)
+    {
+        movementInput = inputValue.Get<Vector2>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        // flip player sprites when moving left or right
+        if (movementInput.x != 0)
+        {
+            playerTransform.localScale = new Vector2(Mathf.Sign(movementInput.x), 1);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = movementInput * (movementSpeed * Time.deltaTime);
+        
+    }
+}
